0da8dfa86cea267502956e281824805acf67bc94
Tozhi_xx1 <javiercaloy1123@gmail.com>
2026-08-21 19:55:49 +0800

    Add files via upload

---
+++ Activity3.py
@@ -0,0 +1,28 @@
+#variables
+#int
+
+age = 22
+year = 2003
+height = 177
+ears = 2
+
+#float
+weight = 67.2
+calories = 44.4
+degree = 35.5 
+bloodsugar = 95.5
+
+#Str
+name = "Carlo"
+surname = "Javier"
+father = "Juan"
+dog = "Tagpi"
+
+#Bool
+isTall = True
+isHandsome = False
+isFat = False
+isGood = True
+
+print(f"my name is {name}")
+ 
