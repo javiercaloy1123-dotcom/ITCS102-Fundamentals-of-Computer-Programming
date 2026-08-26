c833f32d527e63ba46f2e15db46f23df0a9acf35
Tozhi_xx1 <javiercaloy1123@gmail.com>
2026-08-26 21:25:14 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,19 @@
+#operators activity
+
+a = 9
+b = 5
+sum = a + b
+diff = a - b
+prod = a * b
+div = a / b
+expo = a**b
+floordiv = a // b
+percent = a % b
+
+print("the sum of ",a," and",b," is",sum)
+print("the difference of ",a," and", b,"is",diff)
+print("the product of ",a,"  and", b," is",prod)
+print("the divsion of ",a,"  and", b," is",div)
+print("the exponent of ",a," and", b," is",expo)
+print("the floordivision of ",a," and", b," is",floordiv)
+print("the percent of ",a," and", b," is",percent)
