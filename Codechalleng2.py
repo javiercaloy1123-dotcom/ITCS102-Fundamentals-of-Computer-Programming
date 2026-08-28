e61f155cedd37b1a30615ef8d5fa028b940f7b1b
Tozhi_xx1 <javiercaloy1123@gmail.com>
2026-08-28 23:17:15 +0800

    Add files via upload

---
+++ Codechalleng2.py
@@ -0,0 +1,12 @@
+#moneyinthebank
+
+cash1 = 19000 // 1000 
+cash2 = 863 // 500
+cash3 = 363 // 200
+cash4 = 163 // 100
+cash5 = 63 // 50
+cash6 = 0 // 20
+cash7 = 13 // 10
+cash8 = 3 // 1
+
+print("the money will be placed in the bank is according its places,\n",cash1,")
