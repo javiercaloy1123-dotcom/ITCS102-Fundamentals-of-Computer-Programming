017f2a7ad9246918f7854ad7f4bb6b9b8486d667
Tozhi_xx1 <javiercaloy1123@gmail.com>
2026-08-29 00:52:28 +0800

    Add files via upload

---
+++ CodeChallenge1.py
@@ -0,0 +1,6 @@
+#escapesquencemyname
+#\n
+#\t
+
+print("\n\n\n\n\n\t\t\t\t\t\t\t\t\t\t\t\t                   *\n\t\t\t\t\t\t\t\t\t\t\t\t                *     *\n\t\t\t\t\t\t\t\t\t\t\t\t             *           *\n\t\t\t\t\t\t\t\t\t\t\t\t          *                 *\n\t\t\t\t\t\t\t\t\t\t\t\t       *                       *\n\t\t\t\t\t\t\t\t\t\t\t\t    *                             *\n\t\t\t\t\t\t\t\t\t\t\t\t *                                   *\n\t\t\t\t\t\t\t\t\t\t\t\t *                                     *\n\t\t\t\t\t\t\t\t\t\t\t\t * Hi, My Name is Carmelo Carlo Javier *\n\t\t\t\t\t\t\t\t\t\t\t\t *                                     *\n\t\t\t\t\t\t\t\t\t\t\t\t *            From BSIT-1B             *\n\t\t\t\t\t\t\t\t\t\t\t\t *                                     *\n\t\t\t\t\t\t\t\t\t\t\t\t * * * * * * * * * * * * * * * * * * * *\n\t\t\t\t\t\t\t\t\t\t\t\t * * * * * * * * * * * * * * * * * * * *\n\n\n\n\n\n")
+
