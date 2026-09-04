45087706de7630dc87eaf6fa7de1ee7354cae01c
Tozhi_xx1 <javiercaloy1123@gmail.com>
2026-09-04 14:25:52 +0800

    Add files via upload

---
+++ Acivity8.py
@@ -0,0 +1,24 @@
+#pinagkakaabahalaan/hobbies
+
+hobbies = ""#================>>>>>>>>>>>>>>>>>>>>>>>>emptystring<<<<<<<<<<<<<<<<<<<<<<<<<<<========================
+
+b = input("what are your hobbies------------>>>>> ")
+hobbies += b  
+
+b = input("What else----------------------->>>>> ")
+hobbies += b
+
+b = input("What else----------------------->>>>> ")
+hobbies += b
+
+b = input("What else----------------------->>>>> ")
+hobbies += b
+
+b = input("What else----------------------->>>>> ")
+hobbies += b
+
+print("My Hobbies=====>>> are {", hobbies,"}")
+
+
+
+
