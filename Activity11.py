d91bb2057a3990060f8d79b88cdbe96f483f5af8
Tozhi_xx1 <javiercaloy1123@gmail.com>
2026-09-08 20:17:59 +0800

    Add files via upload

---
+++ Activity11.py
@@ -0,0 +1,14 @@
+#Login/Logout/Acess
+
+username = "Carlo Javier"
+password = "mahalko123"
+
+b = input("Input USERNAME==>>")
+a = input("Input PASSWORD==>>")
+
+if b == username or a == password:
+
+       print("username and password are correct")
+
+else:
+      print("Incorrect password or username")
