9b4184ec4b47b3bd75d5f657cfeb23bd06ebcd0f
Tozhi_xx1 <javiercaloy1123@gmail.com>
2026-09-08 20:51:23 +0800

    Add files via upload

---
+++ Activity9.py
@@ -0,0 +1,20 @@
+#relational operator Activity
+
+b = 10
+c = 15
+
+passwordh = "sample"
+passworda = "sample"
+
+#1
+print( b > c )
+#2
+print( b < c )
+#3
+print( b >= c )
+#4
+print( b <= c )
+#5==
+print( b== c)
+print(passwordh == passworda)
+print(passwordh!= passworda)
