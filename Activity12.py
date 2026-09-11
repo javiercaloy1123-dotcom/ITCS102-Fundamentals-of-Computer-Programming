be209c1e546b130c365a0c019d5936cbbe89c6d5
Tozhi_xx1 <javiercaloy1123@gmail.com>
2026-09-11 11:00:23 +0800

    Add files via upload

---
+++ Activity12.py
@@ -0,0 +1,52 @@
+#mulitple if and elif condition
+
+#Create a python progress that would capture age group
+
+name  =  input("Please type your name ====>>>")
+
+age =  int(input("Pleadse type your age ====>>>"))
+
+if  age  >= 0 and age <= 5:
+
+          print("The age is considered as INFANT !!!")
+
+elif age >=  6 and age <=12:
+
+           print("That age considered as KID !!!")
+
+elif age >= 13 and age <= 15:
+
+             print("That age considered as PRE- TEEN !!!")
+
+elif age >= 16 and age <= 19:
+
+     print("That age considered as TEENAGER !!!")
+
+elif age >= 20 and age <= 29:
+
+     print("That age considered  as EARLYA ADULTHOOD !!!")
+
+elif age >= 59 and age <= 150:
+
+       print("That age considered  as SENIOR CITIZEN !!!")
+
+
+else:
+
+ 	print(" YOU ARE DEAD..... 💀...........")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
